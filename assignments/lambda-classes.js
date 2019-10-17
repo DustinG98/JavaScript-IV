4e8476d0f2426b9fb0cc7a1f16ed16bd5df8a750
Dustin Graham <thedustingraham@gmail.com>
2019-10-17 12:24:49 -0600

    created Person class constructor

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,11 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(attr){
+        this.name = attr.name;
+        this.age = attr.age;
+        this.location = attr.location;
+    }
+    speak(){
+        return `Hello my name is ${this.name}, I am from ${this.location}`
+    }
+}//this closes Person class constructor
